83718f1659121d5439bc01ae2574194b19518a55
Dikshit09 <dikshitchoudhary09@gmail.com>
2026-05-19 12:51:38 +0530

    add JWT token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
+const jwt = require("jsonwebtoken")
 const User = require("../model/login_signup")
-const bcryprt = require("bcrypt")
+const bcrypt = require("bcrypt")
 
 const signup = async(req,res) =>{
     try{
@@ -11,18 +12,31 @@ const signup = async(req,res) =>{
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password,salt)
 
-        const newUser = new User.create({
+        const newUser = await User.create({
             name,
             email,
             password:hashedPassword
         })
-        res.status(201).json({message:"User created successfully" })
+        const token = jwt.sign({
+            id: newUser._id,
+            email: newUser.email
+        },process.env.JWT_SECRET,
+        {expiresIn:"7d"}
+    )
+
+    res.status(201).json({
+        message: "User created successfully",
+        token,
+        user:{
+            id: newUser._id,
+            name: newUser.name,
+            email: newUser.email
+        }
+    })
     }catch(err){
         res.status(500).json({message:"Internal server error",err})
     }
 }
-
-
 
 const login = async(req,res) => {
     try{
@@ -30,13 +44,29 @@ const login = async(req,res) => {
 
         const user = await User.findOne({email})
         if(!user){
-            res.status(400).json({message:"Invalid email or password"})
+            return res.status(400).json({message:"Invalid email or password"})
         }
 
         const isMatch =  await bcrypt.compare(password,user.password)
         if(!isMatch){
-            res.status(400).json({message:"Invalid email or password"})
+            return res.status(400).json({message:"Invalid email or password"})
         }
+        const token = jwt.sign({
+            id: user._id,
+            email: user.email
+        },
+    process.env.JWT_SECRET,
+        {expiresIn:"7d"}
+)
+res.status(200).json({
+    message: "Login successful",
+    token,
+    user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+    }
+})    
     }catch(err){
         res.status(500).json({message:"Internal server error",err})
     }
