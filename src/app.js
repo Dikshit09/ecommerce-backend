d86022ca2d7e1b7c4f2b495ac8e82719065349f1
Dikshit09 <dikshitchoudhary09@gmail.com>
2026-05-22 11:52:46 +0530

    add swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const cors = require("cors")
 const authRoutes = require("./routes/authRoutes")
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
 
 const app = express()
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(cors({
     origin: process.env.LIVE_URI ||  "http://localhost:5173",
     credentials:true
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,33 @@ const express = require("express")
 const router = express.Router()
 const {signup,login, getAllUsers,getStats} = require("../controllers/authController")
 
-router.post("/signup",signup)
 router.post("/login",login)
 router.get("/users", getAllUsers)
 router.get('/stats', getStats)
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
+router.post("/signup",signup)
 
 module.exports = router
