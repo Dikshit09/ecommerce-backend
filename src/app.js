767b9f849f95fae5be46c9bcbab78c0606a20cb9
Dikshit09 <dikshitchoudhary09@gmail.com>
2026-05-22 12:09:36 +0530

    fixing swagger error1

--- src/app.js
+++ src/app.js
@@ -6,13 +6,12 @@ const swaggerUi = require('swagger-ui-express')
 
 
 const app = express()
-app.use(express.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(cors({
     origin: process.env.LIVE_URI ||  "http://localhost:5173",
     credentials:true
 }))
-
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use("/api/auth",authRoutes)
 
 
